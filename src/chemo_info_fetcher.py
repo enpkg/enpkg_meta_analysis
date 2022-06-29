54d18b3c65351f64ebf68448e292084f35680a79
Arnaud Gaudry <gaudry.arnaud@gmail.com>
2022-06-29 15:24:08 +0200

    added gnps annotations results in chemo_info_fetcher

--- src/chemo_info_fetcher.py
+++ src/chemo_info_fetcher.py
@@ -10,6 +10,7 @@ from pathlib import Path
 import argparse
 import textwrap
 from pathlib import Path
+from rdkit.Chem import AllChem
 
 p = Path(__file__).parents[1]
 os.chdir(p)
@@ -27,10 +28,13 @@ parser.add_argument('-p', '--sample_dir_path', required=True,
 parser.add_argument('-sql', '--sql_name', default = 'structures_metadata.db',
                     help='The name of a previsouly generated SQL DB (that will be updated with new structures). \
                         If no SQL DB is available, will create new one /output_data/sql_db/')
+parser.add_argument('-id', '--gnps_job_id', required = False, 
+                    help='The GNPS job ID of the meta-MN corresponding to sample_dir_path')
 
 args = parser.parse_args()
 sample_dir_path = args.sample_dir_path
 sql_path = os.path.join(os.getcwd() + '/output_data/sql_db/' + args.sql_name)
+gnps_id = args.gnps_job_id
 
 """ Functions """
 
@@ -66,12 +70,52 @@ def update_sqldb(dataframe, sql_path):
         if conn:
             conn.close()
 
+def get_NPC(short_ik_smiles_query, db_ik, processed_ik, npc_api = "https://npclassifier.ucsd.edu/classify?smiles="):   
+    for sik, smiles in tqdm(short_ik_smiles_query.items(), leave=False):
+        if (sik not in processed_ik) & (sik not in db_ik):
+            processed_ik[sik] = {}
+            processed_ik[sik]['smiles'] = smiles
+            url = npc_api + smiles
+            seperator = "|"
+            try:
+                response = requests.get(url)
+            except HTTPError:
+                processed_ik[sik]['npc_pathway'] = 'unknown'
+                processed_ik[sik]['npc_superclass'] = 'unknown'
+                processed_ik[sik]['npc_class'] = 'unknown'
+                continue                        
+            try:
+                data = response.json()
+            except ValueError:  
+                processed_ik[sik]['npc_pathway'] = 'unknown'
+                processed_ik[sik]['npc_superclass'] = 'unknown'
+                processed_ik[sik]['npc_class'] = 'unknown'
+                continue
+                
+            if len(data['pathway_results']) > 0:
+                joined = seperator.join(data['pathway_results'])
+                processed_ik[sik]['npc_pathway'] = joined
+            else:
+                processed_ik[sik]['npc_pathway'] ='unknown'
+                
+            if len(data['superclass_results']) > 0:
+                joined = seperator.join(data['superclass_results'])
+                processed_ik[sik]['npc_superclass'] = joined
+            else:
+                processed_ik[sik]['npc_superclass'] = 'unknown'
+                
+            if len(data['class_results']) > 0:
+                joined = seperator.join(data['class_results'])
+                processed_ik[sik]['npc_class'] = joined
+            else:
+                processed_ik[sik]['npc_class'] = 'unknown'
+    return  processed_ik
+    
 # These lines allows to make sure that we are placed at the repo directory level 
 p = Path(__file__).parents[2]
 os.chdir(p)
 
 wd_url = 'https://query.wikidata.org/sparql'
-npc_api = "https://npclassifier.ucsd.edu/classify?smiles="
 
 path = os.path.normpath(sample_dir_path)
 samples_dir = [directory for directory in os.listdir(path)]
@@ -89,7 +133,8 @@ else:
     
 # First load all unique short IK from ISDB annotation as long as their metadata (smiles 2D, NPC classes)
 metadata_short_ik = {}
-for directory in samples_dir:
+print('Processing ISDB results')
+for directory in tqdm(samples_dir):
     isdb_path_pos = os.path.join(path, directory, 'pos', 'isdb', directory  + '_isdb_reweighted_flat_pos.tsv')
     isdb_path_neg = os.path.join(path, directory, 'neg', 'isdb', directory  + '_isdb_reweighted_flat_neg.tsv')
     isdb_annotations_pos = None
@@ -117,7 +162,7 @@ for directory in samples_dir:
     else:
         continue
     
-    print(f'Processing ISDB results for sample {directory}')
+    #print(f'Processing ISDB results for sample {directory}')
     
     isdb_annotations.drop_duplicates(subset=['short_inchikey'], inplace=True)
     short_ik = list(isdb_annotations['short_inchikey'])    
@@ -129,9 +174,29 @@ for directory in samples_dir:
             metadata_short_ik[sik]['npc_pathway'] = row['structure_taxonomy_npclassifier_01pathway'].values[0]
             metadata_short_ik[sik]['npc_superclass'] = row['structure_taxonomy_npclassifier_02superclass'].values[0]
             metadata_short_ik[sik]['npc_class'] = row['structure_taxonomy_npclassifier_03class'].values[0]
-    
+
+# Add unique IK from GNPS annotations
+print('Processing GNPS results')
+
+gnps_file = os.listdir(os.path.join(path, '002_gnps', gnps_id, 'result_specnets_DB'))[0]
+gnps_annotations_path = os.path.join(path, '002_gnps', gnps_id, 'result_specnets_DB', gnps_file)
+short_ik_smiles_query = {}
+try:
+    gnps_annotations = pd.read_csv(gnps_annotations_path, sep='\t', usecols=['Smiles', 'InChIKey-Planar'])
+    #print(f'GNPS job found with {len(gnps_annotations)} annotations')
+    for _, row in gnps_annotations.iterrows():
+        mol = AllChem.MolFromSmiles(row["Smiles"])
+        if mol is not None:
+            smiles =  AllChem.MolToSmiles(mol)
+            short_ik_smiles_query[row['InChIKey-Planar']] = smiles
+    metadata_short_ik = get_NPC(short_ik_smiles_query = short_ik_smiles_query, db_ik = short_ik_in_db, processed_ik = metadata_short_ik)
+except FileNotFoundError:
+    pass
+
 # Add unique short IK from Sirius annotations + add NPC metadata
-for directory in samples_dir:
+print('Processing Sirius results')
+
+for directory in tqdm(samples_dir):
     sirius_path_pos = os.path.join(path, directory, 'pos',  directory + '_WORKSPACE_SIRIUS', 'compound_identifications.tsv')
     sirius_path_neg = os.path.join(path, directory, 'neg',  directory + '_WORKSPACE_SIRIUS', 'compound_identifications.tsv')
     sirius_annotations_pos = None
@@ -139,17 +204,14 @@ for directory in samples_dir:
     try:
         sirius_annotations_pos = pd.read_csv(sirius_path_pos, sep='\t')\
             [['InChIkey2D','smiles']]
-        print(len(metadata_short_ik))
     except FileNotFoundError:
         pass
     try:
         sirius_annotations_neg = pd.read_csv(sirius_path_neg, sep='\t')\
             [['InChIkey2D','smiles']]
-
-        print(len(metadata_short_ik))
     except FileNotFoundError:
-        pass 
-        
+        pass    
+
     if (sirius_annotations_pos is not None) & (sirius_annotations_neg is not None):
         sirius_annotations = pd.concat([sirius_annotations_pos, sirius_annotations_neg])
         del(sirius_annotations_pos, sirius_annotations_neg)
@@ -162,53 +224,18 @@ for directory in samples_dir:
     else:
         continue
     
-    print(f'Processing sirius results for sample {directory}')
+    #print(f'Processing sirius results for sample {len(sirius_annotations)}')
     
     sirius_annotations.drop_duplicates(subset=['InChIkey2D'], inplace=True)        
     short_ik = list(sirius_annotations['InChIkey2D'])
-    for sik in tqdm(short_ik):
-        if (sik not in metadata_short_ik) & (sik not in short_ik_in_db):
-            row = sirius_annotations[sirius_annotations['InChIkey2D'] == sik]
-            metadata_short_ik[sik] = {}
-            metadata_short_ik[sik]['smiles'] = row['smiles'].values[0]
-            smiles = row['smiles'].values[0]
-            url = npc_api + smiles
-            seperator = "|"
-            try:
-                response = requests.get(url)
-            except HTTPError:
-                metadata_short_ik[sik]['npc_pathway'] = 'unknown'
-                metadata_short_ik[sik]['npc_superclass'] = 'unknown'
-                metadata_short_ik[sik]['npc_class'] = 'unknown'
-                        
-            try:
-                data = response.json()
-            except ValueError:  
-                metadata_short_ik[sik]['npc_pathway'] = 'unknown'
-                metadata_short_ik[sik]['npc_superclass'] = 'unknown'
-                metadata_short_ik[sik]['npc_class'] = 'unknown'
-                
-            if len(data['pathway_results']) > 0:
-                joined = seperator.join(data['pathway_results'])
-                metadata_short_ik[sik]['npc_pathway'] = joined
-            else:
-                metadata_short_ik[sik]['npc_pathway'] ='unknown'
-                
-            if len(data['superclass_results']) > 0:
-                joined = seperator.join(data['superclass_results'])
-                metadata_short_ik[sik]['npc_superclass'] = joined
-            else:
-                metadata_short_ik[sik]['npc_superclass'] = 'unknown'
-                
-            if len(data['class_results']) > 0:
-                joined = seperator.join(data['class_results'])
-                metadata_short_ik[sik]['npc_class'] = joined
-            else:
-                metadata_short_ik[sik]['npc_class'] = 'unknown'
-
+    short_ik_smiles_query = pd.Series(sirius_annotations.smiles.values,index=sirius_annotations.InChIkey2D).to_dict()
+    metadata_short_ik = get_NPC(short_ik_smiles_query = short_ik_smiles_query, db_ik = short_ik_in_db, processed_ik = metadata_short_ik)
+     
 df_ik_meta = pd.DataFrame.from_dict(metadata_short_ik, orient='index')\
     .reset_index().rename(columns={'index':'short_inchikey'}).fillna('unknown')
-    
+
+print('Getting WD id and formatting results')
+ 
 if len(df_ik_meta) > 0:
     wd_all = get_all_ik(wd_url)
     wd_all['short_inchikey'] = wd_all['inchikey'].str[:14]
